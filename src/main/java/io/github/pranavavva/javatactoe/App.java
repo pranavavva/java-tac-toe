27ca3e458d22c91a23b40caca162a289d46fce6b
Pranav Avva <pranav.avva@gmail.com>
2020-03-27 15:42:12 -0400

    looks pretty

--- src/main/java/io/github/pranavavva/javatactoe/App.java
+++ src/main/java/io/github/pranavavva/javatactoe/App.java
@@ -5,11 +5,27 @@ package io.github.pranavavva.javatactoe;
 
 import io.github.pranavavva.javatactoe.gui.GameFrame;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
         Board board = new Board();
         EventQueue.invokeLater(() -> new GameFrame(board));
     }
